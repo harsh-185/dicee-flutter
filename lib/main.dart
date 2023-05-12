3f9a30b4431745710e975b27014ca6058641857d
harsh-185 <harsh.maheshwari180504@gmail.com>
2023-05-12 11:30:58 +0530

    comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.red[700],
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.black,
         ),
         body: DicePage(),
       ),
@@ -15,9 +18,64 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  var randomNumber1 = 1;
+  var randomNumber2 = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: FilledButton(
+                style: ButtonStyle(
+                    enableFeedback: true,
+                    backgroundColor: MaterialStateProperty.all(
+                        Colors.transparent)
+                    ),
+                onPressed: () {
+                  setState(() {
+                    randomNumber1 = randomize();
+                    randomNumber2 = randomize();
+                  });
+                  },
+                child: Image.asset('images/dice$randomNumber1.png')
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FilledButton(
+                style: ButtonStyle(
+                    enableFeedback: true,
+                    backgroundColor: MaterialStateProperty.all(
+                        Colors.transparent)
+                ),
+                onPressed: () {
+                  setState(() {
+                    randomNumber1 = randomize();
+                    randomNumber2 = randomize();
+                  });
+                  },
+                child: Image.asset('images/dice$randomNumber2.png')
+            ),
+          )
+        ],
+      ),
+    );
   }
+}
+
+
+int randomize() {
+  int randomNumber = Random().nextInt(6) + 1; // generates a random number between 1 and 6
+  return randomNumber;
 }
